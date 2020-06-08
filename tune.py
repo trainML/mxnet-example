2c5988cd0ed5a9d9ade3f029e17df63942cfb1e9
Adam Kowalski <adam@trainml.ai>
2020-06-08 11:45:37 -0500

    initial commit

---
+++ tune.py
@@ -0,0 +1,125 @@
+import pickle
+import time
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+from hyperopt.mongoexp import MongoTrials
+from types import SimpleNamespace
+import sys
+import mxnet as mx
+from math import log
+from gluoncv import utils as gutils
+from gluoncv.model_zoo import get_model
+from gluoncv.data.transforms.presets.rcnn import (
+    FasterRCNNDefaultTrainTransform,
+    FasterRCNNDefaultValTransform,
+)
+from train_faster_rcnn import get_dataset, get_dataloader, train
+
+
+def objective(space):
+
+    sys.setrecursionlimit(1100)
+    args = SimpleNamespace(**space)
+    # fix seed for mxnet, numpy and python builtin random generator.
+    gutils.random.seed(args.seed)
+
+    ctx = [mx.gpu(int(i)) for i in args.gpus.split(",") if i.strip()]
+    ctx = ctx if ctx else [mx.cpu()]
+
+    # training data
+    train_dataset, val_dataset, eval_metric = get_dataset(args.dataset, args)
+
+    # network
+    kwargs = {}
+    module_list = []
+    if args.use_fpn:
+        module_list.append("fpn")
+    if args.norm_layer is not None:
+        module_list.append(args.norm_layer)
+        if args.norm_layer == "syncbn":
+            kwargs["num_devices"] = len(ctx)
+
+    num_gpus = len(ctx)
+    net_name = "_".join(("faster_rcnn", *module_list, args.network, args.dataset))
+
+    net = get_model(
+        net_name,
+        pretrained_base=True,
+        per_device_batch_size=args.batch_size // num_gpus,
+        **kwargs
+    )
+    args.save_prefix += net_name
+
+    for param in net.collect_params().values():
+        if param._data is not None:
+            continue
+        param.initialize()
+    net.collect_params().reset_ctx(ctx)
+    batch_size =  args.batch_size
+    train_data, val_data = get_dataloader(
+        net,
+        train_dataset,
+        val_dataset,
+        FasterRCNNDefaultTrainTransform,
+        FasterRCNNDefaultValTransform,
+        batch_size,
+        len(ctx),
+        args,
+    )
+
+    # training
+    train(net, train_data, val_data, eval_metric, batch_size, ctx, args)
+    name, values = eval_metric.get()
+    idx = name.index('mAP')
+
+    return {
+        "loss": 1 - values[idx],
+        "status": STATUS_OK,
+        "eval_time": time.time(),
+    }
+
+space = {
+    'network': 'resnet50_v1b',
+    'dataset': 'voc',
+    'save_prefix': '',
+    'horovod': False,
+    'amp': False,
+    'resume': False,
+    'start_epoch': 0,
+    'verbose': False,
+    'custom_model': False,
+    'kv_store': 'nccl',
+    'log_interval': 100,
+    'save_interval': 1,
+    'val_interval': 1,
+    'disable_hybridization': False,
+    'static_alloc': False,
+    'seed': 233,
+    'mixup': False,
+    'norm_layer': None,
+    'num_workers': 4,
+    'gpus': '0',
+    'executor_threads': 1,
+    'epochs': 1,
+    'batch_size': 1,
+    'lr': 0.001,
+    'lr_decay': 0.1,
+    'lr_decay_epoch':'14,20',
+    'lr_warmup': -1,
+    'lr_warmup_factor': 1. / 3.,
+    'momentum': hp.uniform('momentum', 0,1),
+    'wd': hp.loguniform('wd', log( 1e-5 ), log( 100 )),
+    'rpn_smoothl1_rho': 1. / 9.,
+    'rcnn_smoothl1_rho': 1.,
+    'use_fpn': hp.choice('use_fpn', [True, False]) 
+}
+
+trials = MongoTrials('mongo://localhost:27017/hyperopt/jobs', exp_key='mxnet_pascal_voc_1')
+best = fmin(
+    objective,
+    space=space,
+    algo=tpe.suggest,
+    max_evals=3,
+    trials=trials,
+)
+
+print(best)
